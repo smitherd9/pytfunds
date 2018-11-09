03237e9dfc4a6c8a977a8441f008fdce1fb61150
Daniel Smither <smitherd9@gmail.com>
2018-11-09 14:37:23 -0500

    start ignoring config/index.js

--- config/index.js
+++
@@ -1,9 +0,0 @@
-const dbuser = 'smitherd9';
-const dbpassword = 'abc123456';
-
-const MONGODB_URI = `mongodb://${dbuser}:${dbpassword}@ds137483.mlab.com:37483/pytfunds`;
-
-module.exports = MONGODB_URI;
-
-
-
